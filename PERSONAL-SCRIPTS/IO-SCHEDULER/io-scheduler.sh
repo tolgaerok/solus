338dc88852c0208d737f5ce9dfa7e8532e0c9c84
Tolga Erok <kingtolga@gmail.com>
2024-10-21 20:54:15 +0800

    (ツ)_/¯ Edit: 21-10-2024 08:54:15 PM

---
+++ PERSONAL-SCRIPTS/IO-SCHEDULER/io-scheduler.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# tolga erok
+
+# Create the systemd service file for setting the I/O scheduler
+echo "[Unit]
+Description=Set I/O Scheduler on boot
+
+[Service]
+Type=simple
+ExecStart=/bin/bash -c 'echo kyber | sudo tee /sys/block/sda/queue/scheduler; printf \"I/O Scheduler set to: \"; cat /sys/block/sda/queue/scheduler'
+
+[Install]
+WantedBy=default.target" | sudo tee /etc/systemd/system/io-scheduler.service
+
+# Reload systemd, enable, and start the service
+sudo systemctl daemon-reload
+sudo systemctl enable io-scheduler.service
+sudo systemctl start io-scheduler.service
+sudo systemctl status io-scheduler.service

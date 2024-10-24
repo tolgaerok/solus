57d4b7113d9c2216eb91e95150a05e5e5d341e39
Tolga Erok <kingtolga@gmail.com>
2024-10-24 19:40:24 +0800

    (ツ)_/¯ Edit: 24-10-2024 07:40:24 PM

--- PERSONAL-SCRIPTS/SYSTEMD-STUFF/MEGASYNC/megasync-systemd-create.sh
+++ PERSONAL-SCRIPTS/SYSTEMD-STUFF/MEGASYNC/megasync-systemd-create.sh
@@ -1,33 +1,28 @@
 #!/bin/bash
 # Tolga Erok
 # 24 oct 2024
-# Make systemD megasync starter
 
-SERVICE_FILE="$HOME/.config/systemd/user/megasync.service"
+# variables
+SERVICE_NAME="megasync.service"
+SERVICE_PATH="$HOME/.config/systemd/user/$SERVICE_NAME"
 
-# Create configuration
-cat <<EOF >"$SERVICE_FILE"
+# Create the systemd configuration
+cat <<EOF >"$SERVICE_PATH"
 [Unit]
-Description=MEGAsync with my container f40
+Description=MEGAsync with my f40 container
 After=graphical.target
 
 [Service]
-ExecStart=/bin/bash -c 'if [ "\$XDG_SESSION_TYPE" = "wayland" ]; then \
-    export XDG_RUNTIME_DIR="/run/user/\$(id -u)"; \
-    export WAYLAND_DISPLAY="wayland-0"; \
-else \
-    export DISPLAY=":0"; \
-fi; \
-distrobox-enter --name f40 -- megasync'
+ExecStart=/bin/bash -c 'if [ "\$XDG_SESSION_TYPE" = "wayland" ]; then export XDG_RUNTIME_DIR="/run/user/\$(id -u)"; export WAYLAND_DISPLAY="wayland-0"; else export DISPLAY=":0"; fi; distrobox-enter --name f40 -- megasync'
 Restart=on-failure
+StandardOutput=journal
+StandardError=journal
 
 [Install]
 WantedBy=default.target
 EOF
 
 # Enable the service's
-systemctl --user enable megasync.service
-systemctl --user start megasync.service
-systemctl --user status megasync.service
-
-echo "MEGAsync service created and started successfully!"
+systemctl --user enable "$SERVICE_NAME"
+systemctl --user start "$SERVICE_NAME"
+systemctl --user status "$SERVICE_NAME"
